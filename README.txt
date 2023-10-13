86e680eb4dde06239b75e30f21ec3085572b6a66
Jonathan Antivo <jonathan.antivo@cabinetsbycomputer.com>
2023-10-13 20:45:12 +0800

    face recognition

---
+++ README.txt
@@ -0,0 +1,3 @@
+KILANGAN NAKA RUN TO SA LOCALHOST
+
+HINDi PWD click mo lang ang index.html, ay gagana na , dapat nasa localhost
---
+++ face.js
@@ -0,0 +1,22 @@
+// Load face-api.js models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+  ]).then(start);
+  
+  // Function to start face detection
+  function start() {
+    const video = document.getElementById('video');
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+  
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+    }, 100);
+  }
